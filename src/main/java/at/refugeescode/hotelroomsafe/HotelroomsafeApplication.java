cc6ff1219ac23c2e41c802353d912d99f4b21099
mohammmod <mohammad.sawas2@outlook.com>
2018-04-23 17:52:27 +0200

    initilize the project

---
+++ src/main/java/at/refugeescode/hotelroomsafe/HotelroomsafeApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.hotelroomsafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelroomsafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelroomsafeApplication.class, args);
+	}
+}
